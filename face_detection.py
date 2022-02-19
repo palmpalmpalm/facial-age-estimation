f531fcdcaeb07c3d54bd274190186fac2c47f708
Palm <supawichs@gmail.com>
2022-02-19 20:56:27 +0700

    Add face_age_estimation and Fix face_detection

--- face_detection.py
+++ face_detection.py
@@ -10,8 +10,8 @@ def main():
         ret, frame = live.read()
         if ret == True:
             gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            coordinate_list = pretrained_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
-            
+            coordinate_list = pretrained_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(30,30))
+
             # draw rectangles
             for (x,y,w,h) in coordinate_list:
                 cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
--- facial_age_estimation.py
+++ facial_age_estimation.py
@@ -1,8 +1,50 @@
 import cv2 as cv
 from tensorflow import keras
+import numpy as np
 
+# load models
 face_detection_model = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 facial_age_estimation_model = keras.models.load_model('models/model1')
+
+
+def get_face_box(frame):
+    
+    # preprocessing
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+    # inferencing
+    detections = face_detection_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(30,30))
+        
+    return detections
+
+def preprocess_for_fae(img):
+    rgb_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    width = 100
+    height = 100
+    resize_img = cv.resize(rgb_img, (width,height), interpolation=cv.INTER_AREA)
+    return np.array(resize_img)/255.0
+
+def facial_age_estimate(frame):
+    
+    # get face bboxess from face_detection_model
+    bboxes = get_face_box(frame)
+    
+    # draw rectangles
+    for (x,y,w,h) in bboxes:
+        cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+    
+    # set padding to 20
+    padding = 20
+    
+    for box in bboxes:
+        # get face frame
+        face = frame[max(0, box[1] - padding):min(box[1] + box[3] + padding, frame.shape[0] - 1),
+               max(0, box[0] - padding):min(box[0] + box[2] + padding, frame.shape[1] - 1)]
+        
+        
+        
+        
+    return frame
 
 
 def main():
@@ -10,17 +52,12 @@ def main():
     while True:
         ret, frame = live.read()
         if ret == True:
-            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            coordinate_list = face_detection_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
-            
-            # draw rectangles
-            for (x,y,w,h) in coordinate_list:
-                cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
             
             # display detected face
-            cv.imshow('live face detection', frame)
+            cv.imshow('live face detection', facial_age_estimate(frame))
             if cv.waitKey(1) == ord('q'):
                 break
+            
     live.release()
     cv.destroyAllWindows()
 
