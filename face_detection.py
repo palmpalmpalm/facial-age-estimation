96ecf6a4bf4dd70b5d9684fda5cb8fa27b7d328f
Supawich Sitdhipol <Supawichs@gmail.com>
2022-02-19 19:42:03 +0700

    first commit

---
+++ face_detection.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+
+pretrained_model = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
+def main():
+    live = cv.VideoCapture(0)
+    while True:
+        ret, frame = live.read()
+        if ret == True:
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            coordinate_list = pretrained_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+            
+            # draw rectangles
+            for (x,y,w,h) in coordinate_list:
+                cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+            
+            # display detected face
+            cv.imshow('live face detection', frame)
+            if cv.waitKey(1) == ord('q'):
+                break
+    live.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
